943153232f87718ec29da74d2f061c57a6c6a7bb
popey2700 <alex.pope@madetech.com>
2024-08-08 22:45:06 +0100

    Add bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import router from './routes';
 
 createRoot(document.getElementById('root')!).render(
