1e8441f585f4dbe44d8de688d8be86739ebb3144
popey2700 <alex.pope@madetech.com>
2024-08-09 00:06:58 +0100

    Adding react imports

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom';
 import LandingPage from './app/pages/landing-page/LandingPage';
-
+import React from 'react';
 const router = createBrowserRouter([
   {
     path: '/',
