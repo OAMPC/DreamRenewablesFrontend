24fc865dce7ad1db8effe81705350e0f9a81decf
popey2700 <alexanderpope27@outlook.com>
2024-08-21 19:55:04 +0100

    Update tests

--- src/components/page-wrapper/PageWrapper.test.tsx
+++ src/components/page-wrapper/PageWrapper.test.tsx
@@ -1,15 +1,26 @@
-import { describe, it, expect, beforeEach } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { describe, it, expect, beforeEach, vi } from 'vitest';
+import { render, screen, waitFor } from '@testing-library/react';
 import React from 'react';
 import PageWrapper from './PageWrapper';
+import mockNavigationBarResponse from '../../test/mock-response/navigation-bar-response.json';
+import axios from 'axios';
+
+vi.mock('axios');
 
 describe('PageWrapper', () => {
   beforeEach(async () => {
+    vi.spyOn(axios, 'get').mockResolvedValue(mockNavigationBarResponse);
     render(
       <PageWrapper>
         <span>Test Content</span>
       </PageWrapper>
     );
+
+    await waitFor(() => {
+      expect(
+        screen.queryByRole('status', { hidden: true })
+      ).not.toBeInTheDocument();
+    });
   });
 
   it('renders the NavigationBar', async () => {
