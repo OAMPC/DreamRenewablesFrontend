9a4ab598436a60b5d1e35118241e090ad8fcef36
popey2700 <alexanderpope27@outlook.com>
2024-08-24 15:03:39 +0100

    Test Api functions

---
+++ src/api/strapiApi.test.ts
@@ -0,0 +1,57 @@
+import { AXIOS_MOCK } from '../test-setup';
+import { describe, test, expect } from 'vitest';
+import { getFooterStrapiData, getNavigationBarStrapiData } from './strapiApi';
+import navigationBarFactory from '../test/builders/strapi/navigationBarFactory';
+import footerFactory from '../test/builders/strapi/footerFactory';
+
+interface MockData<T> {
+  data: {
+    attributes: T;
+  };
+}
+
+describe('strapiApi', () => {
+  const setup = async <T>(
+    apiUrl: string,
+    mockData: MockData<T>,
+    statusCode: number
+  ) => {
+    AXIOS_MOCK.onGet(apiUrl).replyOnce(statusCode, mockData);
+  };
+
+  describe('getNavigationBarStrapiData', () => {
+    test('should get navigation bar data successfully', async () => {
+      const { navBarUrl, mockData } = navigationBarFactory();
+      await setup(navBarUrl, mockData, 200);
+
+      const response = await getNavigationBarStrapiData();
+      expect(response).toEqual(mockData.data.attributes);
+    });
+
+    test('should handle errors when navigation bar get returns a 500', async () => {
+      const { navBarUrl, emptyNavBarMockData } = navigationBarFactory();
+      await setup(navBarUrl, emptyNavBarMockData, 500);
+      await expect(getNavigationBarStrapiData()).rejects.toThrow(
+        'Request failed with status code 500'
+      );
+    });
+  });
+
+  describe('getFooterStrapiData', () => {
+    test('should get footer data successfully', async () => {
+      const { footerUrl, mockData } = footerFactory();
+      await setup(footerUrl, mockData, 200);
+
+      const response = await getFooterStrapiData();
+      expect(response).toEqual(mockData.data.attributes);
+    });
+
+    test('should handle errors when navigation bar get returns a 500', async () => {
+      const { footerUrl, emptyFooterMockData } = footerFactory();
+      await setup(footerUrl, emptyFooterMockData, 500);
+      await expect(getFooterStrapiData()).rejects.toThrow(
+        'Request failed with status code 500'
+      );
+    });
+  });
+});
