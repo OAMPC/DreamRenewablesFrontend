712694203cf39625c60f0e01e912d6372c6103f5
popey2700 <alexanderpope27@outlook.com>
2024-08-24 14:49:28 +0100

    Setup test libraries

--- src/test-setup.ts
+++ src/test-setup.ts
@@ -1,7 +1,22 @@
-import { afterEach } from 'vitest';
+import { afterAll, afterEach, beforeAll, vi } from 'vitest';
 import { cleanup } from '@testing-library/react';
-import '@testing-library/jest-dom/vitest';
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+
+export const AXIOS_MOCK = new MockAdapter(axios);
+
+beforeAll(() => {
+  vi.stubGlobal('console', {
+    ...console,
+    error: vi.fn(),
+  });
+});
 
 afterEach(() => {
+  AXIOS_MOCK.reset();
   cleanup();
+});
+
+afterAll(() => {
+  AXIOS_MOCK.restore();
 });
