b0d01922a420208fb113d115afad0c2b8632d7b6
popey2700 <alexanderpope27@outlook.com>
2024-10-17 20:05:03 +0100

    Create carousel type and update api call

--- src/data/interfaces/landing-page/LandingPageStrapiContent.ts
+++ src/data/interfaces/landing-page/LandingPageStrapiContent.ts
@@ -1,9 +1,11 @@
 import { ImageStrapiContent } from '../util/ImageStrapiContent';
+import { StandardLink } from '../util/StandardLink';
 
 export interface LandingPageStrapiContent {
   landingImageDesktop: LandingImage;
   landingImageMobile: LandingImage;
   videoSection: VideoSection;
+  specialityCarousel: Array<SpecialtyCarouselItem>;
 }
 export interface LandingImage {
   image: ImageStrapiContent;
@@ -14,4 +16,11 @@ export interface VideoSection {
   title: string;
   subTitle: string;
   videoLink: string;
+}
+
+export interface SpecialtyCarouselItem {
+  specialityImage: ImageStrapiContent;
+  specialityTitle: string;
+  specialityDescription: string;
+  link: StandardLink;
 }
