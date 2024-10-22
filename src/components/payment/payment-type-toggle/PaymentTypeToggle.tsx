66b3f478b52521546c661b3693b534a26254d5c0
popey2700 <alexanderpope27@outlook.com>
2024-10-22 18:57:33 +0100

    Add payment type toggle tests

--- src/components/payment/payment-type-toggle/PaymentTypeToggle.tsx
+++ src/components/payment/payment-type-toggle/PaymentTypeToggle.tsx
@@ -6,14 +6,20 @@ const PaymentTypeToggle: React.FC = () => {
   const [paymentType, setPaymentType] = useState('monthly');
   return (
     <>
-      <Bs.ButtonGroup className="payment-type-toggle-wrapper" size="lg">
+      <Bs.ButtonGroup
+        data-testid="payment-type-button-group"
+        className="payment-type-toggle-wrapper"
+        size="lg"
+      >
         <Bs.Button
+          data-testid="payment-type-monthly"
           onClick={() => setPaymentType('monthly')}
           active={paymentType === 'monthly'}
         >
           Monthly
         </Bs.Button>
         <Bs.Button
+          data-testid="payment-type-one-time"
           onClick={() => setPaymentType('oneTime')}
           active={paymentType === 'oneTime'}
         >
