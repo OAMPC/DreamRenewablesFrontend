0f09dee45230b245e89481da18c164312afb3dbe
popey2700 <alexanderpope27@outlook.com>
2024-11-26 11:08:51 +0000

    Simplify string building process

--- src/api/strapiApi.tsx
+++ src/api/strapiApi.tsx
@@ -26,64 +26,64 @@ export async function getStrapiData(endpoint: string, query = {}) {
 }
 
 export async function getNavigationBarStrapiData(): Promise<NavigationBarStrapiContent> {
-  const query = buildStrapiEndpointQuery({
-    'populate[0]': 'brandImage',
-    'populate[1]': 'standardLinks',
-    'populate[2]': 'dropdownLinks.nestedLinks',
-    'populate[3]': 'button',
-  });
+  const query = buildStrapiEndpointQuery([
+    'brandImage',
+    'standardLinks',
+    'dropdownLinks.nestedLinks',
+    'button',
+  ]);
 
   return getStrapiData('navigation-bar', query);
 }
 
 export async function getFooterStrapiData(): Promise<FooterStrapiContent> {
-  const query = buildStrapiEndpointQuery({
-    'populate[0]': 'image',
-    'populate[1]': 'navigationLinks.standardLinks',
-    'populate[2]': 'socialMediaLinks.iconLinks.icon',
-    'populate[3]': 'contactInformation.icon',
-  });
+  const query = buildStrapiEndpointQuery([
+    'image',
+    'navigationLinks.standardLinks',
+    'socialMediaLinks.iconLinks.icon',
+    'contactInformation.icon',
+  ]);
 
   return getStrapiData('footer', query);
 }
 
 export async function getLandingPageStrapiData(): Promise<LandingPageStrapiContent> {
-  const query = buildStrapiEndpointQuery({
-    'populate[0]': 'landingImageDesktop.image',
-    'populate[1]': 'landingImageMobile.image',
-    'populate[2]': 'videoSection',
-    'populate[3]': 'specialitySection.specialityCarousel.image',
-    'populate[4]': 'specialitySection.specialityCarousel.link',
-    'populate[5]': 'specialitySection.specialityCarousel.linkIcon',
-    'populate[6]': 'paymentSection.paymentOptions',
-    'populate[7]': 'paymentSection.paymentOptionIcon',
-    'populate[8]': 'quoteSection.quoteCarousel',
-    'populate[9]': 'quoteSection.quoteIcon',
-  });
+  const query = buildStrapiEndpointQuery([
+    'landingImageDesktop.image',
+    'landingImageMobile.image',
+    'videoSection',
+    'specialitySection.specialityCarousel.image',
+    'specialitySection.specialityCarousel.link',
+    'specialitySection.specialityCarousel.linkIcon',
+    'paymentSection.paymentOptions',
+    'paymentSection.paymentOptionIcon',
+    'quoteSection.quoteCarousel',
+    'quoteSection.quoteIcon',
+  ]);
 
   return getStrapiData('landing-page', query);
 }
 
 export async function getOurMissionVisionAndValuesPageStrapiData(): Promise<OurMissionVisionAndValuesPageStrapiContent> {
-  const query = buildStrapiEndpointQuery({
-    'populate[0]': 'ourMissionSection.titleIcon',
-    'populate[1]': 'ourMissionSection.sectionImage',
-    'populate[2]': 'ourVisionSection.titleIcon',
-    'populate[3]': 'ourVisionSection.sectionImage',
-    'populate[4]': 'ourValuesSection.titleIcon',
-    'populate[5]': 'ourValuesSection.sectionImage',
-    'populate[6]': 'ourValuesSection.ourValuesEntries',
-  });
+  const query = buildStrapiEndpointQuery([
+    'ourMissionSection.titleIcon',
+    'ourMissionSection.sectionImage',
+    'ourVisionSection.titleIcon',
+    'ourVisionSection.sectionImage',
+    'ourValuesSection.titleIcon',
+    'ourValuesSection.sectionImage',
+    'ourValuesSection.ourValuesEntries',
+  ]);
 
   return getStrapiData('mission-vision-and-values-page', query);
 }
 
 export async function getOurTeamPageStrapiData(): Promise<OurTeamPageStrapiContent> {
-  const query = buildStrapiEndpointQuery({
-    'populate[0]': 'departmentSections',
-    'populate[1]': 'departmentSections.teamProfileDetails',
-    'populate[2]': 'departmentSections.teamProfileDetails.profileImage',
-  });
+  const query = buildStrapiEndpointQuery([
+    'departmentSections',
+    'departmentSections.teamProfileDetails',
+    'departmentSections.teamProfileDetails.profileImage',
+  ]);
 
   return getStrapiData('our-team-page', query);
 }
--- src/api/util/buildStrapiEndpointQuery.ts
+++ src/api/util/buildStrapiEndpointQuery.ts
@@ -1,14 +1,5 @@
-export function buildStrapiEndpointQuery(params: {
-  [key: string]: string | string[];
-}): string {
-  return Object.keys(params)
-    .map((key) => {
-      const value = params[key];
-      if (Array.isArray(value)) {
-        return value.map((v) => `${key}=${encodeURIComponent(v)}`).join('&');
-      } else {
-        return `${key}=${encodeURIComponent(value)}`;
-      }
-    })
+export function buildStrapiEndpointQuery(populate: string[]): string {
+  return populate
+    .map((value, index) => `populate[${index}]=${encodeURIComponent(value)}`)
     .join('&');
 }
