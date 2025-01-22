859ed95c264f7662bcc63d9914e3d44f705d4a5c
popey2700 <alexanderpope27@outlook.com>
2025-01-22 16:50:28 +0000

    move footer to scss module

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import * as Bs from 'react-bootstrap';
 import useWindowDimensions from '../../hooks/windowDimensions';
-import './footer.scss';
 import { useSharedData } from '../../contexts/SharedDataProvider';
+import { Col, Nav, Row, Image, Container } from 'react-bootstrap';
+import styles from './footer.module.scss';
 
 const Footer: React.FC = () => {
   const { width } = useWindowDimensions();
@@ -13,99 +13,97 @@ const Footer: React.FC = () => {
     <section data-testid="footer" className="footer-section">
       {content && (
         <footer>
-          <Bs.Container fluid>
+          <Container fluid>
             <hr className="mb-4" />
-            <Bs.Row>
-              <Bs.Col
+            <Row>
+              <Col
                 data-testid="footer-image-col"
                 xl="9"
                 lg="auto"
                 md="12"
                 className={`text-center` + showClass()}
               >
-                <Bs.Row>
-                  <Bs.Col>
-                    <Bs.Image
+                <Row>
+                  <Col>
+                    <Image
                       loading="lazy"
                       data-testid="footer-image"
-                      className="footer-image img-fluid"
+                      className={`${styles.footerImage} img-fluid`}
                       src={content.image.data.attributes.url}
                       alt={content.image.data.attributes.alternativeText}
                     />
-                  </Bs.Col>
-                </Bs.Row>
-                <Bs.Row>
-                  <Bs.Col>
-                    <p className="footer-image-caption">
-                      {content.image.data.attributes.caption}
-                    </p>
-                  </Bs.Col>
-                </Bs.Row>
-                <Bs.Row className="mb-3">
-                  <Bs.Col>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <p>{content.image.data.attributes.caption}</p>
+                  </Col>
+                </Row>
+                <Row className="mb-3">
+                  <Col>
                     <p data-testid="footer-body" className="footer-body">
                       {content.textBody}
                     </p>
-                  </Bs.Col>
-                </Bs.Row>
-              </Bs.Col>
-              <Bs.Col
+                  </Col>
+                </Row>
+              </Col>
+              <Col
                 data-testid="footer-navigation-col"
                 xl="1"
                 lg="auto"
                 md="12"
                 className={`text-center` + showClass()}
               >
-                <Bs.Row>
-                  <Bs.Col>
+                <Row>
+                  <Col>
                     <h3 data-testid="navigation-links-title">
                       <strong>
                         {content.navigationLinks.navigationLinksTitle}
                       </strong>
                     </h3>
-                  </Bs.Col>
-                </Bs.Row>
-                <Bs.Row className="mb-4">
-                  <Bs.Col>
+                  </Col>
+                </Row>
+                <Row className="mb-4">
+                  <Col>
                     {content.navigationLinks.standardLinks.map(
                       (standardLink) => (
-                        <Bs.Nav.Link
+                        <Nav.Link
                           className="my-2"
                           key={standardLink.id}
                           href={standardLink.linkSlug}
                         >
                           {standardLink.linkString}
-                        </Bs.Nav.Link>
+                        </Nav.Link>
                       )
                     )}
-                  </Bs.Col>
-                </Bs.Row>
-              </Bs.Col>
-              <Bs.Col
+                  </Col>
+                </Row>
+              </Col>
+              <Col
                 xl="2"
                 lg="auto"
                 md="12"
                 className={`text-center` + showClass()}
               >
-                <Bs.Row>
-                  <Bs.Col>
+                <Row>
+                  <Col>
                     <h3 data-testid="social-media-links-title">
                       <strong>
                         {content.socialMediaLinks.socialMediaLinksTitle}
                       </strong>
                     </h3>
-                  </Bs.Col>
-                </Bs.Row>
-                <Bs.Row className="mb-5">
-                  <Bs.Col
+                  </Col>
+                </Row>
+                <Row className="mb-5">
+                  <Col
                     className={`${'d-flex' + showClass()} ${'justify-content-center' + showClass()}`}
                   >
                     {content.socialMediaLinks.iconLinks.map((iconLink) => (
-                      <Bs.Image
+                      <Image
                         loading="lazy"
                         data-testid="social-media-icon"
                         key={iconLink.id}
-                        className={`social-media-icon ${'ms-4' + showClass()} me-4`}
+                        className={`${styles.socialMediaIcon} ${'ms-4' + showClass()} me-4`}
                         alt={iconLink.icon.data.attributes.alternativeText}
                         onClick={() =>
                           (window.location.href = iconLink.linkSlug)
@@ -113,26 +111,26 @@ const Footer: React.FC = () => {
                         src={iconLink.icon.data.attributes.url}
                       />
                     ))}
-                  </Bs.Col>
-                </Bs.Row>
-                <Bs.Row>
-                  <Bs.Col>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
                     <h3 data-testid="contact-information-title">
                       <strong>
                         {content.contactInformation.contactInformationTitle}
                       </strong>
                     </h3>
-                  </Bs.Col>
-                </Bs.Row>
-                <Bs.Row>
-                  <Bs.Col>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
                     <h4>{content.contactInformation.contactInformationName}</h4>
-                  </Bs.Col>
-                </Bs.Row>
-                <Bs.Row>
-                  <Bs.Col>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
                     <p>
-                      <Bs.Image
+                      <Image
                         loading="lazy"
                         className="me-1 mb-1"
                         alt={
@@ -145,11 +143,11 @@ const Footer: React.FC = () => {
                       />
                       {content.contactInformation.email}
                     </p>
-                  </Bs.Col>
-                </Bs.Row>
-              </Bs.Col>
-            </Bs.Row>
-          </Bs.Container>
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+          </Container>
         </footer>
       )}
     </section>
