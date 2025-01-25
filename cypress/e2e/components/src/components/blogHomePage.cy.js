f92edad2b8dba25294e053b77985ba32a586423f
12OMarsden <40914332+12OMarsden@users.noreply.github.com>
2025-01-25 21:34:12 +0000

    implemented e2e tests

---
+++ cypress/e2e/components/src/components/blogHomePage.cy.js
@@ -0,0 +1,71 @@
+describe('Blog Home Page', () => {
+  beforeEach(() => {
+    cy.visit('/blog-home');
+
+    cy.wait('@getNavigationBarStrapiData');
+    cy.wait('@getFooterStrapiData');
+    cy.wait('@getOurWorkSubPagesStrapiData');
+    cy.wait('@getBlogPostsStrapiData');
+  });
+
+  it('should load the header and footer and verify all elements are present and functioning', () => {
+    cy.get('[data-testid="navbar"]').should('be.visible');
+    cy.get('[data-testid="footer"]').should('be.visible');
+  });
+
+  it('should load the Blog Home Page and display the title', () => {
+    cy.get('[data-testid="blog-home-page-title"]')
+      .should('be.visible')
+      .and('contain.text', 'Blog Posts');
+  });
+
+  it('should display a grid of blog cards', () => {
+    cy.get('[data-testid="blog-grid"]')
+      .should('be.visible')
+      .find('[data-testid="blog-card"]')
+      .should('have.length.greaterThan', 0);
+  });
+
+  it('should ensure each blog card has necessary elements', () => {
+    cy.get('[data-testid="blog-card"]').each((card) => {
+      cy.wrap(card).within(() => {
+        cy.get('[data-testid^="blog-card-landing-image-"]').should(
+          'be.visible'
+        );
+        cy.get('[data-testid^="blog-card-title-"]').should('be.visible');
+        cy.get('[data-testid^="blog-card-summary-"]').should('be.visible');
+        cy.get('[data-testid^="blog-card-published-time-"]').should(
+          'be.visible'
+        );
+        cy.get('[data-testid^="blog-card-author-"]').should('be.visible');
+      });
+    });
+  });
+
+  it('should verify that clicking a blog card navigates to the correct blog post', () => {
+    cy.get('[data-testid="blog-card-link"]')
+      .first()
+      .then((link) => {
+        const href = link.prop('href');
+        cy.wrap(link).click();
+        cy.url().should('eq', href);
+      });
+  });
+
+  it('should display the blog cards sorted by published date (newest to oldest)', () => {
+    let publishedDates = [];
+    cy.get('[data-testid="blog-card"]')
+      .each((card) => {
+        cy.wrap(card)
+          .find('[data-testid^="blog-card-published-time-"]')
+          .invoke('text')
+          .then((date) => {
+            publishedDates.push(new Date(date));
+          });
+      })
+      .then(() => {
+        const sortedDates = [...publishedDates].sort((a, b) => b - a);
+        expect(publishedDates).to.deep.equal(sortedDates);
+      });
+  });
+});
