9c568de0314fdd2924a7b440c1f494b9cb711dd2
Alex Pope <89132764+popey2700@users.noreply.github.com>
2025-03-29 14:30:35 +0000

    Update our donors page styline (#59)

--- src/components/our-donors-page/ourDonorsPageDonor.tsx
+++ src/components/our-donors-page/ourDonorsPageDonor.tsx
@@ -9,38 +9,41 @@ type Props = {
 
 const OurDonorsPageDonor: React.FC<Props> = ({ ourDonor }) => {
   return (
-    <div className="mb-5" data-testid="our-donor-page-donor">
-      <Row>
-        <Col>
-          <Row>
-            <Col className="d-flex justify-content-center">
-              <div className={styles.logoAccent}>
+    <div
+      className="d-flex flex-column shadow-sm rounded-4 p-3 h-100"
+      data-testid="our-donor-page-donor"
+    >
+      <div className="flex-grow-2 d-flex flex-column">
+        <Row>
+          <Col>
+            <Row>
+              <Col className="d-flex justify-content-center">
                 <Image
                   data-testid="our-donor-page-donor-logo"
-                  className="mb-md-3 rounded-4"
+                  className={`${styles.donorLogo} mb-md-3 rounded-4 shadow-lg mb-4 mb-lg-3`}
                   fluid
                   src={ourDonor.logo.data.attributes.url}
                   alt={ourDonor.logo.data.attributes.alternativeText}
                 />
-              </div>
-            </Col>
-          </Row>
-          <Row>
-            <Col className="text-center text-md-start">
-              <h3 data-testid="our-donor-page-donor-name" className="fw-bold">
-                {ourDonor.name}
-              </h3>
-            </Col>
-          </Row>
-          <Row>
-            <Col className="text-center text-md-start">
-              <p data-testid="our-donor-page-donor-description">
-                {ourDonor.description}
-              </p>
-            </Col>
-          </Row>
-        </Col>
-      </Row>
+              </Col>
+            </Row>
+            <Row>
+              <Col className="text-center">
+                <h3 data-testid="our-donor-page-donor-name" className="fw-bold">
+                  {ourDonor.name}
+                </h3>
+              </Col>
+            </Row>
+            <Row>
+              <Col className="text-center">
+                <p data-testid="our-donor-page-donor-description">
+                  {ourDonor.description}
+                </p>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </div>
     </div>
   );
 };
